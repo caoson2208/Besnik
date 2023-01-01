e03f215f0e26150c378401d1fb5e147685bf67db
Cao Ngọc Sơn <caongocson910@gmail.com>
2023-01-02 03:14:16 +0700

    NS commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/*-----------------------------------
+          GO TO TOP
+-----------------------------------*/
 $(document).ready(function () {
   $("#gototop").hide();
   $(window).scroll(function () {
@@ -17,3 +20,42 @@ $(document).ready(function () {
     );
   });
 });
+
+/*-----------------------------------
+              RUN DATA
+-----------------------------------*/
+var homeSaleTimer = null;
+var homeSoldTimer = null;
+var priceReducedTimer = null;
+$(document).ready(function () {
+  var homeSale = $("#sale");
+  var homeSold = $("#sold");
+  var priceReduced = $("#reduced");
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 2600) {
+      if (homeSaleTimer === null)
+        homeSaleTimer = startData(homeSale, 20, 2500, "sale");
+      if (homeSoldTimer === null)
+        homeSoldTimer = startData(homeSold, 40, 5000, "sold");
+      if (priceReducedTimer === null)
+        priceReducedTimer = startData(priceReduced, 1, 170, "reduced");
+    }
+  });
+});
+
+function startData(element, steps, max, type) {
+  var value = parseInt(element.text());
+  if (value < max)
+    return setInterval(function () {
+      if (value < max) {
+        value += steps;
+        element.text(value);
+      } else {
+        if (type === "sale") clearInterval(homeSaleTimer);
+        else if (type === "sold") clearInterval(homeSoldTimer);
+        else if (type === "reduced") clearInterval(priceReducedTimer);
+        // console.log(Math.random());
+      }
+    }, 10);
+}
