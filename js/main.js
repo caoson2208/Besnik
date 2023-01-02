f8c5bd30e3f2a8ddae5dbd2f91de05f749a60735
Cao Ngọc Sơn <caongocson910@gmail.com>
2023-01-02 14:52:30 +0700

    fix bug

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-/*-----------------------------------
-          GO TO TOP
------------------------------------*/
-$(document).ready(function () {
-  $("#gototop").hide();
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $("#gototop").fadeIn(300);
-    } else {
-      $("#gototop").fadeOut(300);
-    }
-  });
-
-  $("#gototop").click(function () {
-    $("html, body").animate(
-      {
-        scrollTop: 0,
-      },
-      1000
-    );
-  });
-});
 
 /*-----------------------------------
               RUN DATA
